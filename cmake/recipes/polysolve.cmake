e516e5d0f5684b20cfee0901fdcc45f14825c6bf
maxpaik16 <maxpaik@gmail.com>
2025-11-10 19:15:08 -0500

    initial rescaling implementation

--- cmake/recipes/polysolve.cmake
+++ cmake/recipes/polysolve.cmake
@@ -8,4 +8,4 @@ endif()
 message(STATUS "Third-party: creating target 'polysolve'")
 
 include(CPM)
-CPMAddPackage("gh:polyfem/polysolve#71271fb4e231e42fa2cf764c5bf354c81856b105")
+CPMAddPackage("gh:maxpaik16/polysolve#7f398719f7a0565815ef745148a9e615e87cf37a")
