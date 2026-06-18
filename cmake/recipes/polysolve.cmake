de489dd97c47f341e1a19119d1f8f1a2fbab9f81
Ian Su <TzuHsiangSu3000@gmail.com>
2026-06-17 20:17:10 -0400

    point polysolve to local fork

--- cmake/recipes/polysolve.cmake
+++ cmake/recipes/polysolve.cmake
@@ -8,4 +8,8 @@ endif()
 message(STATUS "Third-party: creating target 'polysolve'")
 
 include(CPM)
-CPMAddPackage("gh:polyfem/polysolve#715253d9c9fdf23469cdd6d6f6144717f9c65a51")
+CPMAddPackage(
+    NAME polysolve
+    GIT_REPOSITORY "file://${CMAKE_CURRENT_SOURCE_DIR}/polysolve"
+    GIT_TAG inexact-newton
+)
